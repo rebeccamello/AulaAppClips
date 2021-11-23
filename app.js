b61009c938669c354873c6b4d0c1613d6cfc86e4
Rebecca Mello <rebecca.mello@me.com>
2021-11-23 15:11:47 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express"); // meu app usa o express 
+
+const app = express();
+
+const PORT = process.env.PORT || 3000; // diz qual eh a porta pra acessar as coisas
+
+app.get("/", (req, res) => {
+    res.send("Hello World"); // o que vai printar no site quando abrir
+});
+
+app.listen(PORT, () => {
+    console.log(`App up at port ${PORT}`);
+});
